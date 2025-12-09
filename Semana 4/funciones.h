97dbf685a84f414c5e20269b6067e3a606ae3976
AlexYeipy <alexgp100@gmail.com>
2025-12-09 17:36:09 +0100

    Remove duplicate function declarations in funciones.h

--- Semana 4/funciones.h
+++ Semana 4/funciones.h
@@ -18,28 +18,3 @@ void sendInitialMoveMessage(Player &player, MinimalSocket::udp::Udp<true> &udp_s
 void store_data_see(vector<string> &see_message, Player &player, Ball &ball, Goal &own_goal, Goal &opponent_goal, Field &field);
 
 bool estasentusitio (const Field &field, const Player &player, const Goal &own_goal, const Goal &opponent_goal);
-
-string dash(double power, double angle);
-
-void store_data_hear(string &hear_message, Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &server_udp, Ball &ball);
-
-void chutarPorteria(Player &player, Ball &ball, Goal &opponent_goal, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &server_udp);
-
-JugadorCercano procesarJugadoresVisibles(const vector<string> &see_message, const Player &player);
-
-void mostrarJugadorMasCercano(const JugadorCercano &jugador_mas_cercano);
-
-void configurePlayer(Player &player);
-
-string returnToZone(Player const &player);
-
-void imInZone(Player &player);
-
-void pase(Player &player, Ball &ball,JugadorCercano &jugador, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &server_udp);
-
-void store_data_senseBody(string &senseBody_message, Player &player);
-
-// Función para procesar mensajes 'hear'
-void store_data_hear(string &hear_message, Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &server_udp);
-
-void funcion_modos_juego(const string &modo, Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &server_udp, Ball &ball, Goal &opponent_goal, Boundaries &boundaries);
