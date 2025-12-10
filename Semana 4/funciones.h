de5b744ff190401beaf105f93ec71efde20bf76e
AlexYeipy <alexgp100@gmail.com>
2025-12-10 08:46:59 +0100

    Remove unused function declarations from funciones.h

--- Semana 4/funciones.h
+++ Semana 4/funciones.h
@@ -15,6 +15,4 @@ vector<string> separate_string_separator(string & s, string separator);
 
 void sendInitialMoveMessage(Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &recipient);
 
-void store_data_see(vector<string> &see_message, Player &player, Ball &ball, Goal &own_goal, Goal &opponent_goal, Field &field);
 
-bool estasentusitio (const Field &field, const Player &player, const Goal &own_goal, const Goal &opponent_goal);
